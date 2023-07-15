fe01105430f805e2f5500f75e700ec977507174b
yxing2 <136658270+yxing2@users.noreply.github.com>
2023-07-15 06:44:46 +0000

    3/4 done

--- app.rb
+++ app.rb
@@ -24,3 +24,14 @@ get("/square_root/results") do
   @the_result = @the_num ** 0.5
   erb(:square_root_results)
 end
+
+get("/random/new") do
+  erb(:random_new)
+end
+
+get("/random/results") do
+  @the_min = params.fetch("minimum").to_f
+  @the_max = params.fetch("maximum").to_f
+  @the_result = rand * (@the_max - @the_min) + @the_min
+  erb(:random_results)
+end
