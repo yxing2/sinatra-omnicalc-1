1bc7b2b8dad996d89fc70ccd03c0c8d25e718310
yxing2 <136658270+yxing2@users.noreply.github.com>
2023-07-15 06:37:05 +0000

    2/4 done

--- app.rb
+++ app.rb
@@ -2,8 +2,25 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:hello)
+end
+
+get("/square/new") do
+  erb(:square_new)
+end
+
+get("/square/results") do
+  @the_num = params.fetch("user_number").to_f
+  @the_result = @the_num ** 2
+  erb(:square_results)
+end
+
+get("/square_root/new") do
+  erb(:square_root_new)
+end
+
+get("/square_root/results") do
+  @the_num = params.fetch("square_root_number").to_f
+  @the_result = @the_num ** 0.5
+  erb(:square_root_results)
 end
